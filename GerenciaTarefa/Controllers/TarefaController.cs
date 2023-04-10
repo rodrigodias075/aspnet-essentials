d201b2ce328972628d9178692fba0c4e931fec92
rodrigodias075 <diasmrodrigo5@gmail.com>
2023-04-10 17:43:47 -0300

    criando Tarefa

--- GerenciaTarefa/Controllers/TarefaController.cs
+++ GerenciaTarefa/Controllers/TarefaController.cs
@@ -1,16 +1,18 @@
 
+
 using Microsoft.AspNetCore.Mvc;
 
 namespace GerenciaTarefa.Controllers;
 
 public class TarefaController:Controller{
-//view = Página Web (site)
-//Funções
+   
+   //View = Página Web (site)
+   //Funções
+    public IActionResult Index(){
+        ViewData["Cabecalho"] = "Tarefa Diária";
+        ViewData["Titulo"] = "Planejar Aula Senai";
+        ViewData["Descricao"] = "Criar nova PEUC";
 
-public IActionResult Index(){
-    return View();
-
-
-}
-
+        return View();
+    }
 }
