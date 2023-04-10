29c24e4a8009c0180cd0c507abfae9b325e99e97
rodrigodias075 <diasmrodrigo5@gmail.com>
2023-04-10 16:46:02 -0300

    commitando

---
+++ GerenciaTarefa/Controllers/TarefaController.cs
@@ -0,0 +1,16 @@
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciaTarefa.Controllers;
+
+public class TarefaController:Controller{
+//view = Página Web (site)
+//Funções
+
+public IActionResult Index(){
+    return View();
+
+
+}
+
+}
--- GerenciaTarefa/Models/Tarefa.cs
+++ GerenciaTarefa/Models/Tarefa.cs
@@ -1,8 +1,14 @@
+
+//conjunto de classes
+namespace GerenciaTarefa.Models;
+//conjunto de objetos
 public class Tarefa{
 
 //classe é um conjunto de objetos
 //propriedades automáticas
-public int Id { get; set; }
+public int Id { get; set; } //inteiro
+public string? Descricao { get; set; } //texto
+public bool TaCompleta { get; set; }
 //funcionalidades
 
 
